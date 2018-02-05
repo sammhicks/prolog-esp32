61fa3ac3ca8818884e0f817cefe820964561eb45
Samuel Hicks <sh14820@my.bristol.ac.uk>
2018-02-05 16:32:34 +0000

    Now hosting an AP

--- wam/main/main.cpp
+++ wam/main/main.cpp
@@ -1,22 +1,29 @@
 #include <algorithm>
 
 #include <Arduino.h>
+#include <WiFi.h>
 
-int count;
+const char *ssid = "ESP32 WAM";
+const char *password =
+    "#XAbHWYZodCXLfOm6!a^e^PVi9pqRCH#BlDq#nE4SpvABE20#fBPPNmdMqi8";
+const IPAddress ipAddr(192, 168, 0, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+  case SYSTEM_EVENT_AP_START:
+    WiFi.softAPConfig(ipAddr, ipAddr, subnet);
+    break;
+  default:
+    break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
 
-  delay(1000);
-
-  Serial.println("Hello World");
-
-  count = 0;
+  WiFi.onEvent(WiFiEvent);
+  WiFi.softAP(ssid, password);
 }
 
-void loop() {
-  Serial.println(count, HEX);
-  ++count;
-
-  delay(500);
-}
+void loop() { delay(10); }
